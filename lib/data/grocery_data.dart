9c3e2ef0d72f104aa180f9bfd196b395257fbb19
ayeshaamjad01 <ashamajad007@gmail.com>
2025-03-11 20:36:24 +0500

    logo updated

---
+++ lib/data/grocery_data.dart
@@ -0,0 +1,68 @@
+class GroceryData {
+  static List<Map<String, dynamic>> groceryProducts = [
+    {
+      'id': 1,
+      'name': 'Bananas',
+      'description': 'Fresh Bananas',
+      'price': 2.56,
+      'imageUrl':
+          'https://hips.hearstapps.com/hmg-prod/images/ripe-yellow-bananas-at-the-shopping-market-fruits-royalty-free-image-1712833209.jpg?crop=0.66635xw:1xh;center,top&resize=640:*'
+    },
+    {
+      'id': 2,
+      'name': 'Apples',
+      'description': 'Red Apple',
+      'price': 5.43,
+      'imageUrl':
+          'https://images.contentstack.io/v3/assets/bltcedd8dbd5891265b/blt4bdddb90d9324fc3/667082fe01028f35d4668f1d/types-of-apples-hero.jpg?q=70&width=1200&auto=webp'
+    },
+    {
+      'id': 3,
+      'name': 'Oranges',
+      'description': 'Fresh Oranges',
+      'price': 4.35,
+      'imageUrl':
+          'https://www.health.com/thmb/17kyLFjc_Tx3M-jU2WjNPxXmLJ4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/TypesOfOranges-f95153e786554ba5b6da0370387ee563.jpg'
+    },
+    {
+      'id': 4,
+      'name': 'Grapes',
+      'description': 'Fresh Grapes',
+      'price': 3.24,
+      'imageUrl':
+          'https://asian-veggies.com/cdn/shop/products/tangs2_1296x.png?v=1659936999'
+    },
+    {
+      'id': 5,
+      'name': 'Strawberry',
+      'description': 'Fresh Farm Strawberry',
+      'price': 5.60,
+      'imageUrl':
+          'https://cdn.nyallergy.com/wp-content/uploads/square-1432664914-strawberry-facts1-1200x900.jpeg'
+    },
+    {
+      'id': 6,
+      'name': 'Whole Milk',
+      'description': 'Cow Milk',
+      'price': 4.69,
+      'imageUrl':
+          'https://www.mashed.com/img/gallery/what-you-didnt-know-about-whole-milk/intro-1608135126.jpg'
+    },
+    {
+      'id': 7,
+      'name': 'Cheese',
+      'description': 'Organic Cheese',
+      'price': 5.60,
+      'imageUrl':
+          'https://cheeseorigin.com/wp-content/uploads/2023/09/Organic-Cheese-1024x640.jpeg'
+    },
+    {
+      'id': 8,
+      'name': 'Bread',
+      'description': 'Fresh Bread',
+      'price': 2.45,
+      'imageUrl':
+          'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/paul_hollywoods_crusty_83536_16x9.jpg'
+    },
+  ];
+}
