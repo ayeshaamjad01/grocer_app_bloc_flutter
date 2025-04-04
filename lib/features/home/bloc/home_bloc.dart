4cf190c651b7717c81dadafb56769d5a90ebe3f0
ayeshaamjad01 <ashamajad007@gmail.com>
2025-04-04 15:41:24 +0500

    4-4

--- lib/features/home/bloc/home_bloc.dart
+++ lib/features/home/bloc/home_bloc.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:grocer_app/data/grocery_data.dart';
 import 'package:grocer_app/features/home/models/home_product_data_model.dart';
 
 part 'home_event.dart';
@@ -9,6 +10,7 @@ part 'home_state.dart';
 
 class HomeBloc extends Bloc<HomeEvent, HomeState> {
   HomeBloc() : super(HomeInitial()) {
+    on<HomeInitialEvent>(homeInitialEvent);
     on<HomeProductCartButtonClickedEvent>(homeProductCartButtonClickedEvent);
 
     on<HomeProductWishlistButtonClickedEvent>(
@@ -17,6 +19,22 @@ class HomeBloc extends Bloc<HomeEvent, HomeState> {
     on<HomeWishlistButtonNavigateClickedEvent>(
         homeWishlistButtonNavigateClickedEvent);
   }
+
+  FutureOr<void> homeInitialEvent(
+      HomeInitialEvent event, Emitter<HomeState> emit) async {
+    emit(HomeLoadingState());
+    await Future.delayed(Duration(seconds: 3));
+    emit(HomeLoadedSuccessState(
+        products: GroceryData.groceryProducts
+            .map((e) => ProductDataModel(
+                id: e['id'],
+                name: e['name'],
+                description: e['description'],
+                price: e['price'],
+                imageUrl: e['imageUrl']))
+            .toList()));
+  }
+
   FutureOr<void> homeProductCartButtonClickedEvent(
       HomeProductCartButtonClickedEvent event, Emitter<HomeState> emit) {
     print('Cart Product clicked');
